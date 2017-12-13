3551f4cd762eb66af71ae424beedb6c75f3b6009
joan-esteban <joanestebanr@gmail.com>
2017-12-13 21:23:43 +0100

    - playing with textSwitcher

--- app/src/main/java/org/jesteban/clockomatic/fragments/showdaydetail/DayDetailFragment.java
+++ app/src/main/java/org/jesteban/clockomatic/fragments/showdaydetail/DayDetailFragment.java
@@ -3,10 +3,15 @@ package org.jesteban.clockomatic.fragments.showdaydetail;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Html;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import org.jesteban.clockomatic.R;
 import org.jesteban.clockomatic.fragments.showlistdaysclocks.ShowListDaysClocksFragment;
@@ -20,7 +25,7 @@ public class  DayDetailFragment extends Fragment implements DayDetailContract.Vi
 
     private TextView txtNameDay = null;
     private TextView txtNumDay = null;
-    private TextView txtEntries = null;
+    private TextSwitcher txtEntries = null;
     private TextView txtWorkInfo = null;
     public DayDetailFragment() {
         // Required empty public constructor
@@ -32,8 +37,26 @@ public class  DayDetailFragment extends Fragment implements DayDetailContract.Vi
         View view =  inflater.inflate(R.layout.fragment_view_detail, container, false);
         txtNameDay = (TextView) view.findViewById(R.id.text_name_day);
         txtNumDay = (TextView) view.findViewById(R.id.text_num_day);
-        txtEntries = (TextView) view.findViewById(R.id.text_pair_entries);
+        txtEntries = (TextSwitcher) view.findViewById(R.id.text_pair_entries);
         txtWorkInfo = (TextView) view.findViewById(R.id.text_work_info);
+
+        Animation in = AnimationUtils.loadAnimation(getContext(),
+                android.R.anim.fade_in);
+        Animation out = AnimationUtils.loadAnimation(getContext(),
+                android.R.anim.fade_out);
+
+        txtEntries.setFactory(new ViewSwitcher.ViewFactory() {
+
+            @Override
+            public View makeView() {
+                TextView t = new TextView(getActivity());
+                t.setGravity(Gravity.CENTER);
+                return t;
+            }
+        });
+
+        txtEntries.setInAnimation(in);
+        txtEntries.setOutAnimation(out);
         return view;
     }
 
