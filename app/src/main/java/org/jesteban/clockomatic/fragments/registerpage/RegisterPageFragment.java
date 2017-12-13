dc9a65a8c0929fe44c779deef9e3dffefdbce7b7
joan-esteban <joanestebanr@gmail.com>
2017-12-13 19:07:23 +0100

    - clean code

--- app/src/main/java/org/jesteban/clockomatic/fragments/registerpage/RegisterPageFragment.java
+++ app/src/main/java/org/jesteban/clockomatic/fragments/registerpage/RegisterPageFragment.java
@@ -1,6 +1,5 @@
 package org.jesteban.clockomatic.fragments.registerpage;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
@@ -9,31 +8,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import org.jesteban.clockomatic.R;
-import org.jesteban.clockomatic.StateController;
 import org.jesteban.clockomatic.fragments.registerpage.editclocksday.EditClockDayPresenter;
 import org.jesteban.clockomatic.fragments.reportpage.showdaydetail.DayDetailFragment;
 import org.jesteban.clockomatic.fragments.reportpage.showdaydetail.DayDetailPresenter;
 import org.jesteban.clockomatic.helpers.SewingBox;
 import org.jesteban.clockomatic.views.MyDateTimePickerFragment;
 import org.jesteban.clockomatic.fragments.registerpage.editclocksday.EditClocksDayFragment;
-import org.jesteban.clockomatic.helpers.DependencyInjector;
 import org.jesteban.clockomatic.model.Entry;
 import org.jesteban.clockomatic.model.State;
 
 import java.util.Calendar;
-import java.util.Observable;
+import java.util.NoSuchElementException;
 import java.util.logging.Logger;
 
 
 public class RegisterPageFragment extends Fragment implements MyDateTimePickerFragment.OnMyDateTimePickerFragmentListener,
         RegisterPageContract.View {
     private static final Logger LOGGER = Logger.getLogger(RegisterPageFragment.class.getName());
-    //private StateController stateController = null;
     RegisterPageContract.Presenter presenter = null;
     private MyDateTimePickerFragment myDateTimePickerFragment = null;
-    private EditClocksDayFragment viewClocksDayFragment = null;
-    private DayDetailFragment dayDetailFragment = null;
-    private final DependencyInjector<StateController> stateControllerInjector = new DependencyInjector<>();
 
     public RegisterPageFragment() {
         // Required empty public constructor
@@ -44,25 +37,22 @@ public class RegisterPageFragment extends Fragment implements MyDateTimePickerFr
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_register_page, container, false);
         myDateTimePickerFragment = (MyDateTimePickerFragment) getChildFragmentManager().findFragmentById(R.id.fragment_my_date_time_picker);
-        viewClocksDayFragment = (EditClocksDayFragment) getChildFragmentManager().findFragmentById(R.id.fragment_view_clocks_day);
-        SewingBox.sewPresentersView(new EditClockDayPresenter(viewClocksDayFragment),presenter,viewClocksDayFragment);
-        dayDetailFragment = (DayDetailFragment)getChildFragmentManager().findFragmentById(R.id.fragment_detailed_info_day);
-        if (dayDetailFragment==null){
-            throw new RuntimeException("can't find fragment for day detail");
+        EditClocksDayFragment viewClocksDayFragment = (EditClocksDayFragment) getChildFragmentManager().findFragmentById(R.id.fragment_view_clocks_day);
+        SewingBox.sewPresentersView(new EditClockDayPresenter(viewClocksDayFragment), presenter, viewClocksDayFragment);
+
+        DayDetailFragment dayDetailFragment = (DayDetailFragment) getChildFragmentManager().findFragmentById(R.id.fragment_detailed_info_day);
+        if (dayDetailFragment == null) {
+            throw new NoSuchElementException("can't find fragment for day detail");
         }
-        SewingBox.sewPresentersView(new DayDetailPresenter(dayDetailFragment, getContext()),presenter,dayDetailFragment);
+        SewingBox.sewPresentersView(new DayDetailPresenter(dayDetailFragment, getContext()), presenter, dayDetailFragment);
         return view;
     }
-
-
-
 
 
     @Override
@@ -70,7 +60,7 @@ public class RegisterPageFragment extends Fragment implements MyDateTimePickerFr
         if (presenter != null) {
             Entry entry = new Entry(date);
             if (!presenter.register(entry)) {
-                Snackbar.make(getView(), "ERROR!!!!!!" , Snackbar.LENGTH_LONG)
+                Snackbar.make(getView(), "ERROR!!!!!!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             } else Snackbar.make(getView(), "Register Date " + entry, Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show();
@@ -92,8 +82,6 @@ public class RegisterPageFragment extends Fragment implements MyDateTimePickerFr
     }
 
 
-
-
     @Override
     public void setPresenter(RegisterPageContract.Presenter presenter) {
         this.presenter = presenter;
@@ -103,8 +91,5 @@ public class RegisterPageFragment extends Fragment implements MyDateTimePickerFr
     public void showDate(Calendar date) {
         if (myDateTimePickerFragment != null)
             myDateTimePickerFragment.setDate(date);
-        //if (viewClocksDayFragment != null)
-        //    viewClocksDayFragment.setShowBelongingDayPrefix(state.getFloatingEntry().getBelongingDay());
-
     }
 }
