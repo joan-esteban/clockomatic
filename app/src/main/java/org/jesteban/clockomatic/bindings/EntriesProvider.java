6387977ac418e3791d7f0e99368c2177732d46bf
joan-esteban <joanestebanr@gmail.com>
2017-12-14 22:15:31 +0100

    - add method to get month or day entries

--- app/src/main/java/org/jesteban/clockomatic/bindings/EntriesProvider.java
+++ app/src/main/java/org/jesteban/clockomatic/bindings/EntriesProvider.java
@@ -4,12 +4,17 @@ package org.jesteban.clockomatic.bindings;
 import org.jesteban.clockomatic.model.Entry;
 import org.jesteban.clockomatic.model.EntrySet;
 
+import java.util.Calendar;
+
 public interface EntriesProvider extends  Provider{
     static final String  KEY_PROVIDER="entries";
 
     Boolean register(Entry date);
     Boolean remove(Entry date);
     EntrySet getEntries();
+    EntrySet getEntriesBelongingDay(Calendar day);
+    EntrySet getEntriesBelongingMonth(Calendar day);
+
 
     void subscribe(Listener listner);
 
