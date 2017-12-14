21cf20a48b951c93617df659f6d8975e630629ab
joan-esteban <joanestebanr@gmail.com>
2017-12-14 22:16:22 +0100

    - delete unused imports

--- app/src/main/java/org/jesteban/clockomatic/helpers/PresenterBasicProviderEntriesReady.java
+++ app/src/main/java/org/jesteban/clockomatic/helpers/PresenterBasicProviderEntriesReady.java
@@ -3,13 +3,6 @@ package org.jesteban.clockomatic.helpers;
 import android.support.annotation.NonNull;
 
 import org.jesteban.clockomatic.bindings.EntriesProvider;
-import org.jesteban.clockomatic.bindings.Provider;
-import org.jesteban.clockomatic.controllers.PresenterBase;
-import org.jesteban.clockomatic.fragments.registerpage.editclocksday.EditClockDayContract;
-import org.jesteban.clockomatic.fragments.reportpage.ReportPageContract;
-import org.jesteban.clockomatic.model.Entry;
-
-import java.util.List;
 
 
 public abstract class PresenterBasicProviderEntriesReady<VIEW_CLASS> extends PresenterBasic<VIEW_CLASS> implements EntriesProvider.Listener {
